193750de68861b88329f8a7c2b4a6bc2b3f000e9
ninica0 <santosnico048@gmail.com>
2024-09-25 14:38:03 -0300

    feat: Quase uma calculadora

---
+++ QuaseCalculadora.java
@@ -0,0 +1,31 @@
+public class  QuaseCalculadora {
+
+	public static void main(String[] args) {
+		// Entrada com declaração de variável
+		int num1, num2, res;
+        num1 = 110;
+        num2 = 220;
+
+       //processamento
+       res = num1 + num2;
+       
+       //saída
+       System.out.println("O resultado final da susbtração é:" + res);
+       res = num1 *num2;
+       //processamento
+       System.out.println("O resultado final da multiplicação é: " + res);  
+       
+       //saída
+       res = num1 / num2;
+       
+      //saída
+      System.out.println("O resultado final da divisão é:" + res);
+      
+      //processamento
+      res = num1 % num2;
+      
+     //saída
+     System.out.println("O resultado final do resto da divisão é: " + res);
+
+}
+}
